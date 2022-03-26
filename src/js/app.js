39b342f04e8b735e05b096d826fce7c4e69123b9
gpb <gpiratebob@gmail.com>
2022-03-26 19:56:00 +0800

    nuke test log

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,6 @@ window.$ = window.jQuery = require('jquery');
 import "foundation-sites";
 import "what-input";
 import "./modules/_form.js"
- 
+
 $(document).foundation();
-console.log(`Testing jQuery, this should be a one: ${$('body').length}`);
+
